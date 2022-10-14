336646faa40ffa87b273792524b28422fa1ebede
EvgSuslov <94070733+EvgSuslov@users.noreply.github.com>
2022-10-14 13:10:06 +0300

    Create Generating_bracket_sequences.py

---
+++ Generating_bracket_sequences.py
@@ -0,0 +1,16 @@
+n = int(input())
+n *= 2
+for mask in range(1 << n):
+    balance, is_good, answer = 0, True, ''
+    for bit in range(n):
+        if (mask >> bit) & 1:
+            balance += 1
+            answer += '('
+        else:
+            balance -= 1
+            if balance < 0:
+                is_good = False
+                break
+            answer += ')'
+    if is_good and balance == 0:
+        print(answer)
